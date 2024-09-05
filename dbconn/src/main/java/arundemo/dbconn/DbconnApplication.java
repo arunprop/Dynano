5e25dd6d2ca890fb7fc6435788f0b8d8483f94f5
arunprop <35284916+arunprop@users.noreply.github.com>
2024-09-04 21:09:35 -0500

    Add files via upload
    Initial Project creation

---
+++ dbconn/src/main/java/arundemo/dbconn/DbconnApplication.java
@@ -0,0 +1,13 @@
+package arundemo.dbconn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbconnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbconnApplication.class, args);
+    }
+
+}
---
+++ dbconn/src/test/java/arundemo/dbconn/DbconnApplicationTests.java
@@ -0,0 +1,13 @@
+package arundemo.dbconn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbconnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
